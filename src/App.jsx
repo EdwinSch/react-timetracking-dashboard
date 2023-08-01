50e28125b648a9b981f394bbf6cb32ef23ee9c75
front-ed <edwinschutjes@gmail.com>
2023-08-01 17:54:47 +0200

    refactor dataset

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import trackingData from "./data.json";
+
 function App() {
+  const [data, setData] = useState(trackingData);
+  const [activeIndex, setActiveIndex] = useState(2);
+
   return (
     <>
       <h2>hello world</h2>
+      <div>
+        {data.map((item, index) => {
+          const { title, timeframes } = item;
+
+          return (
+            <div className="card" key={index}>
+              <h4>{title}</h4>
+              <p>{timeframes[activeIndex].current}</p>
+              <p>{timeframes[activeIndex].previous}</p>
+
+              {/* {console.log(timeframes)} */}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
