b4b9d54ebbc65699dd52b8436cd942149bfbcdeb
EdwinSch <edwinschutjes@gmail.com>
2023-08-02 11:34:53 +0200

    style: person info

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,7 +1,19 @@
+import avatar from "../assets/image-jeremy.png";
+
 const General = () => {
   return (
     <section className="general-container">
-      <p>report</p>
+      <div className="person">
+        <img src={avatar} alt="profile picture" />
+        <p>Report for</p>
+        <h1>
+          jeremy <br /> robson
+        </h1>
+      </div>
+      {/* todo: button component */}
+      <div className="button-container">
+        <button>bla</button>
+      </div>
     </section>
   );
 };
